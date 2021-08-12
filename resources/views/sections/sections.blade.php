e9a76bd270fabf1bc4a8dd5d5dd29d6fea230649
wolf <ahmedragabyasin2020@gmail.com>
2021-08-12 19:12:16 +0200

    change font and add jquery translate

--- resources/views/sections/sections.blade.php
+++ resources/views/sections/sections.blade.php
@@ -47,7 +47,7 @@
 
 @if (session()->has('delete'))
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <strong>{{ session()->get('delete') }}</strong>
+        <strong style="padding-right: 35px;">{{ session()->get('delete') }}</strong>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
@@ -56,7 +56,7 @@
 
 @if (session()->has('edit'))
     <div class="alert alert-success alert-dismissible fade show" role="alert">
-        <strong>{{ session()->get('edit') }}</strong>
+        <strong style="padding-right: 35px;">{{ session()->get('edit') }}</strong>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
